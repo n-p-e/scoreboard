d1b77e2f14efc4f3c14bfd22237a33ff41c96150
n-p-e <dev@nullpointererror.com>
2025-08-08 19:44:02 +1000

    improve link type

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,21 +1,30 @@
 import { createLink, LinkComponent } from "@tanstack/solid-router"
 import * as Solid from "solid-js"
 
-type BasicLinkProps = Solid.JSX.IntrinsicElements["a"] & {
+type CustomLinkProps = Solid.JSX.IntrinsicElements["a"] & {
   activeClass?: string
   inactiveClass?: string
 }
 
-const BasicLinkComponent: Solid.Component<BasicLinkProps> = (props) => {
-  return <a {...props}>{props.children}</a>
+const BasicLinkComponent: Solid.Component<
+  CustomLinkProps & { isActive?: boolean }
+> = (props) => {
+  const [, linkProps] = Solid.splitProps(props, [
+    "activeClass",
+    "inactiveClass",
+    "isActive",
+  ])
+  return <a {...linkProps}>{props.children}</a>
 }
 
 const CreatedLinkComponent = createLink(BasicLinkComponent)
 
-export const Link: LinkComponent<typeof BasicLinkComponent> = (props) => {
+export const Link: LinkComponent<Solid.Component<CustomLinkProps>> = (
+  props
+) => {
+  // The styles and classes are merged by tanstack router
   return (
     <CreatedLinkComponent
-      preload={"intent"}
       activeProps={{
         class: props.activeClass,
       }}
