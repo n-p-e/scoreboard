1c3df754529708835816d3d2f86798e83d1622ac
n-p-e <dev@nullpointererror.com>
2026-04-24 14:34:02 +1000

    port error handler logic

--- src/server/backend.ts
+++ src/server/backend.ts
@@ -3,10 +3,13 @@ import { leaguesHandler } from "~/league/leagues-handler"
 import { riichiHandler } from "~/riichi/riichi-handler"
 import { usersHandler } from "~/users/users-handler"
 import { authMiddleware } from "./auth-middleware"
+import { backendErrorHandler, notFoundHandler } from "./error-handler"
 import { HonoEnv } from "./server-types"
 
 const apiApp = new Hono<HonoEnv>()
   .use("*", authMiddleware)
+  .onError(backendErrorHandler)
+  .notFound(notFoundHandler)
   .get("/healthcheck", async (c) => c.json({ status: "success" }))
   .route("/", usersHandler)
   .route("/", leaguesHandler)
